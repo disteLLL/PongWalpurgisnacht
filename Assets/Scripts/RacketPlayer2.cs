34bd51ed3bab2c3e9476bf5a1101d9c9210fa4fc
Tim Fock <tim.fock@googlemail.com>
2018-04-23 14:55:50 +0200

    cleanup

--- Assets/Scripts/RacketPlayer2.cs
+++ Assets/Scripts/RacketPlayer2.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class RacketPlayer2 : MonoBehaviour {
 
-    public float movementSpeed;
+    //public float movementSpeed;
     public CollectionController collectionController;
 
-    private void FixedUpdate() {
+    /*private void FixedUpdate() {
         float v = Input.GetAxisRaw("Vertical2");
         GetComponent<Rigidbody2D>().velocity = new Vector2(0, v) * movementSpeed;
-    }
+    }*/
 
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.gameObject.tag == "Collectable") {
