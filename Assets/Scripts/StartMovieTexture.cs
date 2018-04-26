02a853214eb32e3adaa0b3572f3acfd65ee37703
Tim Fock <tim.fock@googlemail.com>
2018-04-26 20:01:11 +0200

    movie textures with loop

---
+++ Assets/Scripts/StartMovieTexture.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMovieTexture : MonoBehaviour {
+
+    private MovieTexture movie;
+
+	// Use this for initialization
+	void Start () {
+        movie = (MovieTexture)GetComponent<Renderer>().material.mainTexture;
+        movie.loop = true;
+        movie.Play();
+    }
+}
